3283987b43d70962873a953186ccdb215ec7cbb2
Anand Phulwani <anand_phulwani@yahoo.com>
2023-10-14 02:18:46 +0530

    Some minor changes and changing of order.

--- main.py
+++ main.py
@@ -1274,8 +1274,8 @@ def topOrNot():
         if windows.isMaximized:
             if isOverlayEnabled:
                 window.lower()
-                window.attributes("-topmost", False)
                 overlay.lower()
+                window.attributes("-topmost", False)                
                 overlay.attributes("-topmost", False)
             else:
                 window.lower()
@@ -1285,7 +1285,7 @@ def topOrNot():
                 "Choose a note:" and windows.title != "Save your note:" and
                 windows.title != "Choose an Image:" and windows.title != "tk"):
             if isOverlayEnabled:
-                # overlay.deiconify()
+                overlay.deiconify()
                 overlay.attributes("-topmost", False)
                 overlay.lower()
                 window.deiconify()
@@ -1307,7 +1307,7 @@ def topOrNot():
         elif windows.title == "tk":
             if isOverlayEnabled:
                 window.attributes("-topmost", False)
-                # overlay.deiconify()
+                overlay.deiconify()
                 overlay.lift()
                 overlay.attributes("-topmost", True)
             else:
