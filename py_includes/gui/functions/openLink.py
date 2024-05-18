8dfb29647f361f69e4757443680140c292de08e8
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-19 05:17:40 +0530

    Checked if link tag is in selection if not return, if present putting cursor anywhere in the middle of link and click openURL will open the link.

--- py_includes/gui/functions/openLink.py
+++ py_includes/gui/functions/openLink.py
@@ -1,11 +1,30 @@
 ﻿import webbrowser
 from ... import globalvars
+import tkinter
 
 def openLink(_=False):
     """Open link button function"""
-
-    current_tags = globalvars.notes.tag_names("sel.first")
+    
+    # Get the index of the current cursor position
+    cursor_index = globalvars.notes.index(tkinter.INSERT)
+    
+    # Get all tags at the cursor position
+    current_tags = globalvars.notes.tag_names(cursor_index)
+    
     if "link" in current_tags:
-        url = globalvars.notes.selection_get()
+        # Find the start and end indices of the link
+        start = cursor_index
+        while "link" in globalvars.notes.tag_names(f"{start} -1c"):
+            start = globalvars.notes.index(f"{start} -1c")
+        end = cursor_index
+        while "link" in globalvars.notes.tag_names(f"{end} +1c"):
+            end = globalvars.notes.index(f"{end} +1c")
+        end = globalvars.notes.index(f"{end} +1c")
+        
+        # Select the full link
+        url = globalvars.notes.get(start, end)
+        
+        # Open the URL in a web browser
         webbrowser.open_new(url)
+        
     return "break"
