4754f8d787771a1625f481703a398f777a796f88
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-24 06:46:10 +0530

    Increased fn `check_and_set_window_to_top_or_bottom` loop calls from 500 to 100, to make it more faster.

--- py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
+++ py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
@@ -26,4 +26,4 @@ def check_and_set_window_to_top_or_bottom():
         # Setting the desktop as the foreground window
         hwnd_desktop = ctypes.windll.user32.GetDesktopWindow()
         ctypes.windll.user32.SetForegroundWindow(hwnd_desktop)
-    globalvars.window.after(500, check_and_set_window_to_top_or_bottom)
+    globalvars.window.after(100, check_and_set_window_to_top_or_bottom)
