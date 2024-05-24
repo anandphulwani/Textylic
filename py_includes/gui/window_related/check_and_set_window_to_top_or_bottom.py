dfdbb18c14412460e0b6dcc29c678da3ddfd806f
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-24 06:57:58 +0530

    Repalced `if z_order == 'donothing': pass` with `if z_order != 'donothing':`.

--- py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
+++ py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
@@ -15,8 +15,7 @@ def check_and_set_window_to_top_or_bottom():
     hwnd = get_hwnd(globalvars.window)
     z_order = get_z_order(hwnd)
     
-    if z_order == 'donothing':
-        pass
+    if z_order != 'donothing':
     if z_order == 'forcetop':
         win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_TOPMOST)
         win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOACTIVATE)
