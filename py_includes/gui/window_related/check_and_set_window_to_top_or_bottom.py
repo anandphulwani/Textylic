c58ebd9bcc6ede9176f9638e08910664ce0cf72c
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-25 04:26:15 +0530

    Added `current_focus_mode` is `lockapp` functionality.

--- py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
+++ py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
@@ -3,6 +3,8 @@ from ...helpers.add_always_on_top import add_always_on_top
 from ...helpers.remove_always_on_top import remove_always_on_top
 from ...helpers.is_topmost import is_topmost
 from ...helpers.get_hwnd import get_hwnd
+from ...helpers.get_executable_name import get_executable_name
+from ...helpers.get_window_class_name import get_window_class_name
 from ...helpers.get_z_order import get_z_order
 from ... import globalvars
 
@@ -18,6 +20,18 @@ def check_and_set_window_to_top_or_bottom():
     if globalvars.current_focus_mode == "lock" and not is_topmost(hwnd):
         globalvars.focus_lockapp_window = None
         add_always_on_top(hwnd)
+    elif globalvars.current_focus_mode == "lockapp":
+        hwnd_top = globalvars.user32.GetForegroundWindow()
+        if not (get_executable_name(hwnd_top) == 'C:\\Windows\\explorer.exe' and get_window_class_name(hwnd_top) == 'Shell_TrayWnd'):
+            if hwnd_top != "" and hwnd_top != hwnd:
+                if globalvars.focus_lockapp_window == None:
+                    globalvars.focus_lockapp_window = hwnd_top
+                if hwnd_top == globalvars.focus_lockapp_window and not is_topmost(hwnd):
+                    add_always_on_top(hwnd)
+                else:
+                    remove_always_on_top(hwnd)
+        else:
+            remove_always_on_top(hwnd)
     elif globalvars.current_focus_mode == "unlock":
         globalvars.focus_lockapp_window = None
         z_order = get_z_order(hwnd)
