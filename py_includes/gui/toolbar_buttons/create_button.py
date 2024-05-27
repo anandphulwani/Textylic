05f9575eb8849a16be1e49fa5532860608361a2f
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-28 03:29:52 +0530

    Shifted `bg` and `active_bg` as positional parameters in `create_button` fn.

--- py_includes/gui/toolbar_buttons/create_button.py
+++ py_includes/gui/toolbar_buttons/create_button.py
@@ -1,7 +1,7 @@
 ﻿import tkinter
 from ... import globalvars
 
-def create_button(parent, image, command, row, column, padx, pady, sticky="W", bg="#181926", active_bg="#181926", bd=0, fg="white", add_to_accent_items=False):
+def create_button(parent, image, command, row, column, padx, pady, bg, active_bg, sticky="W", bd=0, fg="white", add_to_accent_items=False):
     button = tkinter.Button(parent, image=image, bd=bd, bg=bg, command=command, activebackground=active_bg, fg=fg, padx=3, pady=4)
     button.image = image
     button.grid(row=row, column=column, padx=padx, pady=pady, sticky=sticky)
