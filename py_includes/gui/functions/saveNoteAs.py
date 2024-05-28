c12f5efcfe58f967b0d83bf97740929d75bbac5e
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-29 03:15:44 +0530

    Auto indentation only.

--- py_includes/gui/functions/saveNoteAs.py
+++ py_includes/gui/functions/saveNoteAs.py
@@ -17,12 +17,12 @@ def saveNoteAs(_=False):
     )
     if not noteFile:
         return
-        globalvars.saved = True
-        globalvars.openedFileName = noteFile
-        noteFile = open(os.path.join(globalvars.dataPath, noteFile), "w")
-        noteFile.write(globalvars.notes.get(1.0, "end"))
-        noteFile.close()
-        # Messagebox
-        openedFileNameStrip = re.sub("C:/.*/", "", str(globalvars.openedFileName))
-        tkinter.messagebox.showinfo(" ", f'Successfully saved note as "{openedFileNameStrip}"   ')
+    globalvars.saved = True
+    globalvars.openedFileName = noteFile
+    noteFile = open(os.path.join(globalvars.dataPath, noteFile), "w")
+    noteFile.write(globalvars.notes.get(1.0, "end"))
+    noteFile.close()
+    # Messagebox
+    openedFileNameStrip = re.sub("C:/.*/", "", str(globalvars.openedFileName))
+    tkinter.messagebox.showinfo(" ", f'Successfully saved note as "{openedFileNameStrip}"   ')
     return "break"
