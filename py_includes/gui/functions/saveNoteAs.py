32d17759405ceb95260e263cbae6976ad6b8669a
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-29 03:26:02 +0530

    Auto indentation only.

--- py_includes/gui/functions/saveNoteAs.py
+++ py_includes/gui/functions/saveNoteAs.py
@@ -10,20 +10,20 @@ def saveNoteAs(_=False):
 
     with globalvars.save_fn_lock:
         if not globalvars.saved:
-        noteFile = filedialog.asksaveasfilename(
-            confirmoverwrite=True,
-            defaultextension=".txtlyc",
-            filetypes=(("Textylic file", "*.txtlyc"),),
-            initialdir = globalvars.dataPath,
-            title="Save your note:",
-        )
-        if not noteFile:
-            return
-        os.rename(globalvars.openedFileName, noteFile)
-        globalvars.openedFileName = noteFile
+            noteFile = filedialog.asksaveasfilename(
+                confirmoverwrite=True,
+                defaultextension=".txtlyc",
+                filetypes=(("Textylic file", "*.txtlyc"),),
+                initialdir = globalvars.dataPath,
+                title="Save your note:",
+            )
+            if not noteFile:
+                return
+            os.rename(globalvars.openedFileName, noteFile)
+            globalvars.openedFileName = noteFile
         saveNote()
         if not globalvars.saved:
-        # Messagebox
-        openedFileNameStrip = re.sub("C:/.*/", "", str(globalvars.openedFileName))
-        tkinter.messagebox.showinfo(" ", f'Successfully saved note as "{openedFileNameStrip}"   ')
-        globalvars.saved = True
+            # Messagebox
+            openedFileNameStrip = re.sub("C:/.*/", "", str(globalvars.openedFileName))
+            tkinter.messagebox.showinfo(" ", f'Successfully saved note as "{openedFileNameStrip}"   ')
+            globalvars.saved = True
