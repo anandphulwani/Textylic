a39e00e09c6d6f2718349c3abb6ceb42a43b5af7
Anand Phulwani <anand_phulwani@yahoo.com>
2024-05-29 14:07:40 +0530

    Corrected onhover effect not working when image of focus is changed.

--- py_includes/gui/functions/changeFocusSetting.py
+++ py_includes/gui/functions/changeFocusSetting.py
@@ -1,4 +1,5 @@
 from ... import globalvars
+from ...images_effects import bind_hover_effects
 
 def changeFocusSetting(_=False):
     """Change the focus setting and update button appearance."""
@@ -7,15 +8,19 @@ def changeFocusSetting(_=False):
     if current_mode == "unlock":
         globalvars.current_focus_mode = "lock"
         new_image = globalvars.window.focuslockButtonImage
+        hover_image = globalvars.window.focuslockButtonImageAfter
     elif current_mode == "lock":
         globalvars.current_focus_mode = "pinapp"
         new_image = globalvars.window.focuspinappButtonImage
+        hover_image = globalvars.window.focuspinappButtonImageAfter
     else:
         globalvars.current_focus_mode = "unlock"
         new_image = globalvars.window.focusunlockButtonImage
+        hover_image = globalvars.window.focusunlockButtonImageAfter
         
     
     # Update the button image and background color
     button = globalvars.top_menu_buttons["focusunlock"]
     button.config(image=new_image)
+    bind_hover_effects(button, hover_image, new_image)
     button.image = new_image  # Prevent garbage collection
