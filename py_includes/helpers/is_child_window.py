6d316605c57ea5924991fd5d1c18dd78629a0694
Anand Phulwani <anand_phulwani@yahoo.com>
2024-06-02 04:40:38 +0530

    Replaced `get_executable_name` with `get_process_id`.

--- py_includes/helpers/is_child_window.py
+++ py_includes/helpers/is_child_window.py
@@ -1,8 +1,10 @@
+from .. import globalvars
 from ..helpers.get_executable_name import get_executable_name
+from ..helpers.get_process_id import get_process_id
 
 def is_child_window(child_hwnd, parent_hwnd):
     if child_hwnd == 0 or parent_hwnd == 0:
         return False
-    if get_executable_name(child_hwnd) == get_executable_name(parent_hwnd):
+    if get_process_id(child_hwnd) == globalvars.parent_pid:
         return True
     return False
