281c95a65c4cd468cba4005980f181b3576534f9
Anand Phulwani <anand_phulwani@yahoo.com>
2024-06-03 05:09:38 +0530

    Auto indentation only.

--- py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
+++ py_includes/gui/window_related/check_and_set_window_to_top_or_bottom.py
@@ -23,30 +23,30 @@ def check_and_set_window_to_top_or_bottom():
         (get_executable_name(hwnd_top) == 'C:\\Windows\\explorer.exe' and get_window_class_name(hwnd_top) == 'TaskListThumbnailWnd'):
         pass
     else:
-    if globalvars.current_focus_mode == "lock" and not is_topmost(globalvars.parent_hwnd):
-        globalvars.focus_pinapp_window = None
-        add_always_on_top(globalvars.parent_hwnd)
-    elif globalvars.current_focus_mode == "pinapp":
-            if hwnd_top != "" and hwnd_top != globalvars.parent_hwnd:
-                if globalvars.focus_pinapp_window == None:
-                    globalvars.focus_pinapp_window = hwnd_top
-                if hwnd_top == globalvars.focus_pinapp_window and not is_topmost(globalvars.parent_hwnd):
-                    add_always_on_top(globalvars.parent_hwnd)
-                else:
-                    remove_always_on_top(globalvars.parent_hwnd)
-        else:
+        if globalvars.current_focus_mode == "lock" and not is_topmost(globalvars.parent_hwnd):
+            globalvars.focus_pinapp_window = None
+            add_always_on_top(globalvars.parent_hwnd)
+        elif globalvars.current_focus_mode == "pinapp":
+                if hwnd_top != "" and hwnd_top != globalvars.parent_hwnd:
+                    if globalvars.focus_pinapp_window == None:
+                        globalvars.focus_pinapp_window = hwnd_top
+                    if hwnd_top == globalvars.focus_pinapp_window and not is_topmost(globalvars.parent_hwnd):
+                        add_always_on_top(globalvars.parent_hwnd)
+                    else:
+                        remove_always_on_top(globalvars.parent_hwnd)
+            else:
+                remove_always_on_top(globalvars.parent_hwnd)
+        elif globalvars.current_focus_mode == "unlock":
+            globalvars.focus_pinapp_window = None
             remove_always_on_top(globalvars.parent_hwnd)
-    elif globalvars.current_focus_mode == "unlock":
-        globalvars.focus_pinapp_window = None
-        remove_always_on_top(globalvars.parent_hwnd)
-        z_order = get_action_according_to_zorder(globalvars.parent_hwnd)
-        if z_order != 'donothing':
-            if z_order == 'forcetop':
-                add_always_on_top(globalvars.parent_hwnd)
-            elif globalvars.window_is_focused == False and z_order != 'bottom':
-                # Setting the window to the bottom
-                ctypes.windll.user32.SetWindowPos(globalvars.parent_hwnd, 1, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE)
-                # Setting the desktop as the foreground window
-                hwnd_desktop = ctypes.windll.user32.GetDesktopWindow()
-                ctypes.windll.user32.SetForegroundWindow(hwnd_desktop)
+            z_order = get_action_according_to_zorder(globalvars.parent_hwnd)
+            if z_order != 'donothing':
+                if z_order == 'forcetop':
+                    add_always_on_top(globalvars.parent_hwnd)
+                elif globalvars.window_is_focused == False and z_order != 'bottom':
+                    # Setting the window to the bottom
+                    ctypes.windll.user32.SetWindowPos(globalvars.parent_hwnd, 1, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE)
+                    # Setting the desktop as the foreground window
+                    hwnd_desktop = ctypes.windll.user32.GetDesktopWindow()
+                    ctypes.windll.user32.SetForegroundWindow(hwnd_desktop)
     globalvars.window.after(100, check_and_set_window_to_top_or_bottom)
