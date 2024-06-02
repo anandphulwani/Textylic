05a79f94ff671e8de57a23618a759bf4876674d7
Anand Phulwani <anand_phulwani@yahoo.com>
2024-06-03 05:12:05 +0530

    Corrected `remove_always_on_top.py` bug where window is not going to bottom.

--- py_includes/helpers/remove_always_on_top.py
+++ py_includes/helpers/remove_always_on_top.py
@@ -5,4 +5,4 @@ def remove_always_on_top(hwnd):
     ex_style = win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE)
     ex_style = ex_style & ~win32con.WS_EX_TOPMOST
     win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, ex_style)
-    win32gui.SetWindowPos(hwnd, win32con.HWND_NOTOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOACTIVATE)
+    win32gui.SetWindowPos(hwnd, win32con.HWND_BOTTOM, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOACTIVATE)
